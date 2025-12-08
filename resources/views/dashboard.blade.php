f68d0eea933c7e95428fa608e474bbed8125dcc0
Arlanzon <arlanzon.fernando@gmail.com>
2025-12-08 14:20:05 -0600

    Nuevos cambios de diseño

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl leading-tight">
             {{ __('Panel de Control') }} - <span
                 class="text-blue-500">{{ ucfirst(Auth::user()->getRoleNames()->first()) }}</span>
         </h2>
